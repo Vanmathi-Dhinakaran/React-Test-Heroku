145700f8d99534011e44fdd3c3f0462cba0d1e14
Vanmathi-Dhinakaran <mathi21dhina@gmail.com>
2022-09-18 20:26:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Posts from "./components/Posts";
+import { ReactComponent as Logo } from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo style={{ height: 200 }} />
+        <h1>React Posts Sharer</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
